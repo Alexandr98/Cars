650871fec06df3d140c1e52b49ece620c68592a0
Sasha98 <sashaprim98@gmail.com>
2018-08-27 17:40:47 +0300

    something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cars, Car } from './car.model';
+import { Store } from '@ngrx/store';
+import { AppState } from './redux/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public cars: Car[] = [];
+
+  public constructor(private store: Store<AppState>) {
+
+  }
+
+  ngOnInit() {
+    
+  }
 
  public add(car: Car) {
     this.cars.push(car);
